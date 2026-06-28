343093397b9e401ac1c8c8f77e72191454545f1d
Ezequias Ribamar <ezequiasribamar3@gmail.com>
2026-06-28 17:40:04 -0300

    adicionando a funcionalidade do cad.py

--- VibeShowPoetry/src/vibe_show/Controller/sistema_controller.py
+++ VibeShowPoetry/src/vibe_show/Controller/sistema_controller.py
@@ -10,8 +10,29 @@ class SistemaController:
         while True:
 
             opcao = self.view.MenuPrincipal()   
+            if opcao == '1':
+                self.gerenciar_usuario()
 
-            if opcao == "1":
+            elif opcao == "2":
+                print("Ver histórico de compras... (implementar na model/view)")
+                
+            elif opcao == "3":
+                print("Comprar ingressos... (implementar na model/view)")
+                
+            elif opcao == "4":
+                print("Escolher um local... (implementar na model/view)")
+                
+            elif opcao == "5":
+                print("Solicitando reembolso... (implementar na model/view)")
+                
+            else:
+                print("Opção inválida no Menu Principal.")
+
+
+    def gerenciar_usuario(self):
+        while True:
+            opcao_user = self.view.MenuUsuario()
+            if opcao_user == "1":
 
                 nome = input("Digite seu nome: ")
                 email = input("Digite seu email: ")
@@ -20,22 +41,22 @@ class SistemaController:
                 self.model.cadastrar(nome, email, senha)
 
 
-            elif opcao == "2":
+            elif opcao_user == "2":
 
                 email = input("Digite seu email: ")
                 senha = input("Digite sua senha: ")
 
                 self.model.login(email, senha)
 
-            elif opcao == "3":
+            elif opcao_user == "3":
 
                 email = input("Digite seu email: ")
 
                 self.model.recuperar_senha(email)
 
-            elif opcao == "4":
+            elif opcao_user == "4":
 
-                print("Encerrando sistema...")
+                print("Voltando ao menu principal")
                 break
 
             else:
